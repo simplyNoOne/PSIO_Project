f796618b04ee54a9877bf7342c80f8694e713e83
rychu2115 <272643@student.pwr.edu.pl>
2022-12-16 12:33:49 +0100

    getters and setters

---
+++ src/Character.java
@@ -0,0 +1,57 @@
+public abstract class Character
+{
+    private String name;
+    private int health;
+    private int armor;
+    private int baseDamage;
+    private int criticalChance;
+    private int dodgeChance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getBaseDamage() {
+        return baseDamage;
+    }
+
+    public void setBaseDamage(int baseDamage) {
+        this.baseDamage = baseDamage;
+    }
+
+    public int getCriticalChance() {
+        return criticalChance;
+    }
+
+    public void setCriticalChance(int criticalChance) {
+        this.criticalChance = criticalChance;
+    }
+
+    public int getDodgeChance() {
+        return dodgeChance;
+    }
+
+    public void setDodgeChance(int dodgeChance) {
+        this.dodgeChance = dodgeChance;
+    }
+}
