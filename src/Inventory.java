a9927d6026118db250350019ac477c4e14112266
simplyNoOne <82574004+simplyNoOne@users.noreply.github.com>
2022-12-17 02:11:45 +0100

    Player weapons (#2) (#3)
    * player_weapons_base_func
    
    * player_weapons_base_func_constr
    
    Co-authored-by: maxon461 <116277191+maxon461@users.noreply.github.com>

---
+++ src/Inventory.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Inventory {
+    private ArrayList<Weapon> weapons = new ArrayList<Weapon>();
+    // private ArrayList<Collectible> collectibles = new ArrayList<Collectible>();          For Wlad!!!!!!
+
+    public int getNumOfWeapons(){
+        return this.weapons.size();
+    }
+
+    public void addWeapon(Weapon weapon){
+        weapons.add(weapon);
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public Weapon getWeaponAt(int x){
+        return this.weapons.get(x);
+    }
+
+    public void deleteWeaponAt(int x){
+        weapons.remove(weapons.get(x));
+    }
+
+
+    
+
+    
+    
+}
