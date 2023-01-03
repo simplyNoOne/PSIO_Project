dcb0cf199f5d69ae6607c96480ab5e8f2515a5c7
Ania000 <annasidor00@gmail.com>
2023-01-03 14:57:11 +0100

    displaying fight progress

--- src/managers/FightManager.java
+++ src/managers/FightManager.java
@@ -14,103 +14,94 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class FightManager {
+
+    private static boolean isBeginning;
+
+    private static double elapsedTime;
+    static int rounds;
     private static String message;
     private static Character attacker; // abstract handle, can change between Player and Enemy. Convenient to use as a class field
     private static Character defender; // the other character
     private static Boolean playerWon = null; // Fight result. Wrapping as a non-primitive boolean gives possibility to set result as null when it's unknown
     private static final int DAMAGE_RANDOMIZATION_PERCENT = 10; // Damage slightly randomized e.g. base hit 100 will really be a random from 90 ... 110
 
-    public static class WonFightButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            StateMachine.setNextStateVar(StateMachine.State.SCROLL_BG);
-            StateMachine.nextState();
-        }
-    }
 
-    public static class LostFightButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
 
-            StateMachine.setNextStateVar(StateMachine.State.FINAL_RESULTS);
-            StateMachine.nextState();
-        }
-    }
-
-    public static class WonAndLevelupFightButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            StateMachine.setNextStateVar(StateMachine.State.LEVELUP);
-            StateMachine.nextState();
-        }
-    }
-
-    private static final WonFightButtonListener wonFightButtonListener = new WonFightButtonListener();
-    private static final LostFightButtonListener lostFightButtonListener = new LostFightButtonListener();
-
-    private static final WonAndLevelupFightButtonListener wonAndLevelupFightButtonListener = new WonAndLevelupFightButtonListener();
 
 
     public static void init() {
-        ((Interactible)GUIManager.getPanel("fight")).addButtonListener(wonFightButtonListener, "won");
-        ((Interactible)GUIManager.getPanel("fight")).addButtonListener(lostFightButtonListener, "lost");
-        ((Interactible)GUIManager.getPanel("fight")).addButtonListener(wonAndLevelupFightButtonListener, "levelup");
+        message = "Opponents approach each other...";
+        rounds = 0;
+        ((FightPanel) GUIManager.getPanel("fight")).setMessage(message);
     }
 
 
 
-    public static Boolean hasPlayerWon() {
-        return playerWon;
+
+
+
+    public static void fightRound(double dT) {
+
+
+        elapsedTime += dT;
+        if(elapsedTime > 2) {
+            elapsedTime = 0;
+            if(playerWon != null)
+                fightFinished();
+            else {
+                isBeginning = !isBeginning;
+                if (isBeginning) {
+                    rounds++;
+                    if (rounds == 1) {
+                        resetFightResult();
+                        // Setup who attacks first and the other one defends. Based on flipping a coin (50/50 chance).
+                        chooseAttackerAndDefender();
+                    } else
+                        switchAttacker();
+
+                    System.out.println("Fight!");
+
+                    message = "Now " + attacker.getName() + " charges...";
+                    ((FightPanel) GUIManager.getPanel("fight")).setMessage(message);
+
+                    MainApp.getGameFrame().repaint();
+
+                } else {
+                    message = "";
+                    if (hasDefenderDodged()) {
+                        message = defender.getName() + " has dodged the attack!";
+                    } else {
+                        dealDamage();
+                    }
+                    if (defender instanceof Player)
+                        ((StatsPanel) GUIManager.getPanel("playerStats")).updateStats();
+                    else
+                        ((StatsPanel) GUIManager.getPanel("enemyStats")).updateStats();
+
+                    ((FightPanel) GUIManager.getPanel("fight")).setMessage(message);
+                    MainApp.getGameFrame().repaint();
+
+                    setFightResult();
+
+                }
+            }
+        }
     }
 
-    public static void startFight() {
-        resetFightResult();
-
-        // Handles for explicit objects info display e.g. player & enemy health
-        Player player = MainApp.getPlayer();
-        Enemy enemy = MainApp.getEnemy();
-
-        // Setup who attacks first and the other one defends. Based on flipping a coin (50/50 chance).
-        chooseAttackerAndDefender();
-
-        System.out.println("Fight!");
-        while (attacker.getHealth() > 0 & defender.getHealth() > 0) {
-            message = "Now " + attacker.getName() + " hits...";
-            ((FightPanel)GUIManager.getPanel("fight")).setMessage(message);
-            GUIManager.getPanel("fight").revalidate();
-            MainApp.getGameFrame().repaint();
-            try {
-                Thread.sleep(1500);
-            }catch (InterruptedException ex){
-                ex.printStackTrace();
-            }
-
-            if (hasDefenderDodged()) {
-                message = defender.getName() + " has dodged the attack!";
-            } else {
-                dealDamage();
-            }
-            if( defender instanceof Player)
-                ((StatsPanel)GUIManager.getPanel("playerStats")).updateStats();
+    private static void fightFinished(){
+        if (playerWon) {
+            if(!((Enemy)defender).isCanPuzzle())
+                StateMachine.setNextStateVar(StateMachine.State.LEVELUP);
             else
-                ((StatsPanel)GUIManager.getPanel("enemyStats")).updateStats();
+                StateMachine.setNextStateVar(StateMachine.State.SCROLL_BG);
 
-            ((FightPanel)GUIManager.getPanel("fight")).setMessage(message);
-            GUIManager.getPanel("fight").revalidate();
-            MainApp.getGameFrame().repaint();
-            System.out.println();
-            switchAttacker();
-        }
-
-        setFightResult();
-        if (hasPlayerWon()) {
-           message = "Player won!";
-        }
-        else message = "Enemy won!";
+        } else
+            StateMachine.setNextStateVar(StateMachine.State.FINAL_RESULTS);
 
         fightCleanup();
+        StateMachine.nextState();
     }
+
 
     private static void resetFightResult() {
         playerWon = null;
@@ -155,10 +146,12 @@ public class FightManager {
 
         if (damage > 0) {
             defender.setHealth(defender.getHealth() - damage);
-            message += String.format("Boom! %s (%s) hits %s (%s) with %d damage!\n", attacker.getName(), attacker.getClass(), defender.getName(), defender.getClass(), damage);
+            if(defender.getHealth() < 0)
+                defender.setHealth(0);
+            message += String.format("Boom! %s hits %s with %d damage!\n", attacker.getName(),  defender.getName(), damage);
         }
         else {
-            message += String.format("%s (%s)'s armor has blocked the attack!\n", defender.getName(), defender.getClass());
+            message += String.format("%s 's armor has blocked the attack!\n", defender.getName());
         }
     }
 
@@ -234,5 +227,9 @@ public class FightManager {
     private static void fightCleanup() {
         attacker = null;
         defender = null;
+        rounds = 0;
+        isBeginning = false;
+        message = "Opponents approach each other...";
+        ((FightPanel) GUIManager.getPanel("fight")).setMessage(message);
     }
 }
