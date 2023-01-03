e420d8bcbc56f35bc79566eb09cb0b9e86243f60
voluxus <voluxus@protonmail.com>
2023-01-03 10:01:31 +0100

    Score board GUI in progress. * moved Back button in score panel a little lower * new class ScoreLabel (in progress) TODO: add scoreLabels to the panel and order ScoreManager to feed them with data, adjust UI for better look

--- src/gui/panels/ScoresPanel.java
+++ src/gui/panels/ScoresPanel.java
@@ -7,12 +7,15 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicBorders;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ScoresPanel extends CustomPanel implements Interactible {
 
     static class ScoresButton extends JButton {
         static final int BUTTON_WIDTH = 170;
         static final int BUTTON_HEIGHT = 40;
+        static final int POS_X = (PANEL_WIDTH - ScoresButton.BUTTON_WIDTH) / 2;
+        static final int POS_Y = PANEL_HEIGHT - POS_X;
 
         ScoresButton() {
             super();
@@ -31,8 +34,25 @@ public class ScoresPanel extends CustomPanel implements Interactible {
         }
     }
 
+    public class ScoreLabel extends JLabel {
+        static final int WIDTH = (int) (0.8f * ScoresPanel.PANEL_WIDTH);
+        static final int HEIGHT = calcHeight();
+        ScoreLabel() {
+            this.setBackground(Color.white);
+            this.setForeground(Color.black);
+            this.setSize(WIDTH, HEIGHT);
+            this.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+            // TODO continue here, need to add labels to the panel
+        }
+        static int calcHeight() {
+            int verticalWorkingSpace = ScoresPanel.PANEL_HEIGHT - ScoresButton.POS_Y;
+            return (ScoresPanel.HEIGHT - verticalWorkingSpace) / MAX_SCORES_ON_PANEL;
+        }
+    }
+
     static int PANEL_WIDTH = 400;
     private static int PANEL_HEIGHT = 500;
+    private static final int MAX_SCORES_ON_PANEL = 6;
 
     ScoresButton backButton = new ScoresButton("Back");
 
@@ -40,7 +60,9 @@ public class ScoresPanel extends CustomPanel implements Interactible {
         super();
         this.setBounds((GUIManager.getWidth() - PANEL_WIDTH)/2, (GUIManager.getHeight() - PANEL_HEIGHT)/2, PANEL_WIDTH, PANEL_HEIGHT);
         //this.setPreferredSize(new Dimension(600, 600));
-        backButton.setBounds((PANEL_WIDTH - ScoresButton.BUTTON_WIDTH) / 2, (PANEL_HEIGHT - ScoresButton.BUTTON_HEIGHT) / 2, ScoresButton.BUTTON_WIDTH, ScoresButton.BUTTON_HEIGHT);
+
+        backButton.setBounds(ScoresButton.POS_X, ScoresButton.POS_Y, ScoresButton.BUTTON_WIDTH, ScoresButton.BUTTON_HEIGHT);
+
         this.add(backButton);
         this.setBackground(Color.red);
         this.setVisible(true);
