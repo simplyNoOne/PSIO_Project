ea0403484e5fc1e0aa4cc4bdbd73b254cab4b476
maxon461 <116277191+maxon461@users.noreply.github.com>
2023-01-05 11:19:26 +0100

    Puzzle (#17)
    * modyfikacja puzzle panel
    
    * Modified PuzzlePanel and PuzzleManager. Now Quiz is displaying correctly on a PuzzlePanel. Added file with some quizzes and allowed PuzzleManager to load all puzzles from this file. quiz file path -> "resources/questions.txt".
    
    Co-authored-by: vshulakk <vlad4545624@gmail.com>
    Co-authored-by: Jędrzej Sobków <272651@student.pwr.edu.pl>

---
+++ src/data/Puzzle.java
@@ -0,0 +1,40 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Puzzle {
+    private String question;
+    private ArrayList<String> answers = new ArrayList<>();
+    private String correct_answer;
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void addAnswer(String answer) {
+        this.answers.add(answer);
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrect_answer() {
+        return correct_answer;
+    }
+
+    public void setCorrect_answer(String correct_answer) {
+        this.correct_answer = correct_answer;
+    }
+
+}
--- src/managers/PuzzleManager.java
+++ src/managers/PuzzleManager.java
@@ -1,40 +1,109 @@
 package managers;
 
+import data.Puzzle;
+import gui.panels.PuzzlePanel;
 import interfaces.Interactible;
 import main.StateMachine;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 public class PuzzleManager {
 
-    public static class CorrectSolutionButtonListener implements ActionListener
-    {
+    public static class AnswerButtonListener implements ActionListener{
+
         @Override
         public void actionPerformed(ActionEvent e) {
-            StateMachine.setNextStateVar(StateMachine.State.SCROLL_BG);
-            StateMachine.nextState();
+            if(((PuzzlePanel.PuzzleButton) e.getSource()).isEnabled()) {
+                String answer = ((JButton) e.getSource()).getText();
+                if (answer.equals(actualPuzzle.getCorrect_answer()))
+                    StateMachine.setNextStateVar(StateMachine.State.SCROLL_BG);
+                else
+                    StateMachine.setNextStateVar(StateMachine.State.PREFIGHT);
+
+                StateMachine.nextState();
+            }
         }
     }
 
-    public static class WrongSolutionButtonListener implements ActionListener
-    {
-        @Override
-        public void actionPerformed(ActionEvent e) {
 
-            StateMachine.setNextStateVar(StateMachine.State.PREFIGHT);
-            StateMachine.nextState();
-        }
-    }
+    private static final AnswerButtonListener answerButtonListener = new AnswerButtonListener();
 
-    private static final CorrectSolutionButtonListener correctSolutionButtonListener = new CorrectSolutionButtonListener();
-    private static final WrongSolutionButtonListener wrongSolutionButtonListener = new WrongSolutionButtonListener();
-
+    private static ArrayList<Puzzle> allQuestions = loadAllQuestions();
+    private static Puzzle actualPuzzle;
 
 
     public static void init() {
-        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(correctSolutionButtonListener, "correct");
-        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(wrongSolutionButtonListener, "wrong");
+        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(answerButtonListener, "0");
+        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(answerButtonListener, "1");
+        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(answerButtonListener, "2");
+        ((Interactible)GUIManager.getPanel("puzzle")).addButtonListener(answerButtonListener, "3");
     }
+
+    public static void refreshPuzzle(){
+        ((PuzzlePanel)GUIManager.getPanel("puzzle")).setAllButtonsAsInactive();
+        generateRandomPuzzle();
+        ArrayList<String> answers = actualPuzzle.getAnswers();
+        for(int answerId = 0; answerId < answers.size(); answerId++)
+        {
+            ((PuzzlePanel)GUIManager.getPanel("puzzle")).setAnswerButtonText(Integer.toString(answerId), answers.get(answerId));
+        }
+        ((PuzzlePanel)GUIManager.getPanel("puzzle")).setQuestionContent(actualPuzzle.getQuestion());
+    }
+
+    public static void generateRandomPuzzle()
+    {
+        if(allQuestions.size() == 1)
+            actualPuzzle = allQuestions.get(0);
+        else {
+            int puzzleId = new Random().nextInt(0, allQuestions.size());
+            actualPuzzle = allQuestions.get(puzzleId);
+        }
+
+        Collections.shuffle(actualPuzzle.getAnswers());
+
+        allQuestions.remove(actualPuzzle);
+        if(allQuestions.size() == 0)
+            allQuestions = loadAllQuestions();
+    }
+
+
+    private static ArrayList<Puzzle> loadAllQuestions() {
+
+        ArrayList<Puzzle> puzzleList = new ArrayList<>();
+        try {
+            File questionsFile = new File("resources\\questions.txt");
+            Scanner myReader = new Scanner(questionsFile);
+            while (myReader.hasNextLine()) {
+                String row = myReader.nextLine();
+                String[] rowTable = row.split("\t");
+                String question = rowTable[0];
+                String[] answers = rowTable[1].replace("<", "").replace(">", "").split(";");
+                int correctAnswerId = Integer.parseInt(rowTable[2]);
+                Puzzle puzzle = new Puzzle();
+                puzzle.setQuestion(question);
+                for (String answer : answers) puzzle.addAnswer(answer);
+                puzzle.setCorrect_answer(answers[correctAnswerId]);
+                puzzleList.add(puzzle);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+    return puzzleList;
+    }
+
+
 
 }
