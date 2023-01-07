6cc2e5ec5c4ce944aefc37e95c23632b9613705d
maxon461 <116277191+maxon461@users.noreply.github.com>
2023-01-07 11:41:00 +0100

    Graphic puzzle (#21)
    * new puzzles
    
    * added graphic puzzle. Still not working correctly
    
    Co-authored-by: vshulakk <vlad4545624@gmail.com>
    Co-authored-by: Jędrzej Sobków <272651@student.pwr.edu.pl>

--- src/main/StateMachine.java
+++ src/main/StateMachine.java
@@ -92,14 +92,13 @@ public class StateMachine {
         },
         PUZZLE{
             public void initState(){
-                GUIManager.addPanel("puzzle", "game");
                 MainApp.getGameFrame().setVisible(true);
-                PuzzleManager.refreshPuzzle();
+                PuzzleManager.newPuzzle();
             }
             public void update(double deltaTime) {}
 
             public void nextState() {
-                GUIManager.removePanel("puzzle", "game");
+                GUIManager.removePanel(PuzzleManager.getPuzzleType(), "game");
                 StateMachine.setCurrentState(PUZZLE_RESULTS);
                 currentState.initState();
             }
